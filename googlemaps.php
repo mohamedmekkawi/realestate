c0676b537831351341b71cb081102276602e1757
mohamedmekkawi <mo2menuc@gmail.com>
2017-11-12 00:01:15 -0500

    google maps

---
+++ googlemaps.php
@@ -0,0 +1,28 @@
+<?php
+
+$app->get('/property/googlemaps', function() use ($app) {
+   if (!$_SESSION['user']) {
+        $app->render("access_denied.html.twig");
+        return;
+    }
+   $userId = $_SESSION['user']['userId'];
+   $propertyList = DB::query("SELECT * FROM property WHERE userId =%i", $userId);
+    if (!$propertyList) {
+        $app->render("/not_found.html.twig");
+        return;
+    }
+    $app->render("/property/google_maps_list.html.twig", array('list'=>$propertyList));
+});
+
+$app->get('/property/googlemap/:id', function($id) use ($app) {
+   if (!$_SESSION['user']) {
+        $app->render("access_denied.html.twig");
+        return;
+    }
+    $property = DB::queryFirstRow("SELECT * FROM property WHERE propertyId=%d AND userId=%i", $id, $_SESSION['user']['userId']);
+    if (!$property) {
+        $app->render("/not_found.html.twig");
+        return;
+    }
+    $app->render("/property/google_maps.html.twig", array('property'=>$property));
+});
